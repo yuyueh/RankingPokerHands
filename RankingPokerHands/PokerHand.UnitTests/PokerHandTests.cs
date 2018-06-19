e06051808397c6e6f9c19e36395f9dc2f54cda0b
ed chang <ed.chang@xuenn.com>
2018-06-19 18:39:09 +0800

    Initialize

---
+++ RankingPokerHands/PokerHand.UnitTests/PokerHandTests.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace PokerHand.UnitTests
+{
+    [TestFixture]
+    public class PokerHandTests
+    {
+        [Test]
+        public void FirstTest()
+        {
+        }
+    }
+}
---
+++ RankingPokerHands/RankingPokerHands/PokerHand .cs
@@ -0,0 +1,6 @@
+﻿namespace RankingPokerHands
+{
+    public class PokerHand 
+    {
+    }
+}
