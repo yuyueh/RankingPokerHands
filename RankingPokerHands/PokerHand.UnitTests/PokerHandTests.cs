0693d1a74e054061f0a1aa55028889e3e1d74b80
ed chang <ed.chang@xuenn.com>
2018-06-20 14:33:18 +0800

    Test_StraightFlushWinsOf4OfAKind

--- RankingPokerHands/PokerHand.UnitTests/PokerHandTests.cs
+++ RankingPokerHands/PokerHand.UnitTests/PokerHandTests.cs
@@ -19,5 +19,20 @@ namespace RankingPokerHands.UnitTests
             // Assert
             Assert.AreEqual(result, expected);
         }
+
+        [Test]
+        public void Test_StraightFlushWinsOf4OfAKind()
+        {
+            // Arrange
+            var target = new PokerHand("2H 3H 4H 5H 6H");
+            var opponentHand = new PokerHand("AS AD AC AH JD");
+            var expected = Result.Win;
+
+            // Act
+            var result = target.CompareWith(opponentHand);
+
+            // Assert
+            Assert.AreEqual(result, expected);
+        }
     }
 }
