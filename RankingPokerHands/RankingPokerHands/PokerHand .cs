b79563746fc2143b6ef09ceacd34302437924d10
ed chang <ed.chang@xuenn.com>
2018-06-20 14:27:50 +0800

    Implement

--- RankingPokerHands/RankingPokerHands/PokerHand .cs
+++ RankingPokerHands/RankingPokerHands/PokerHand .cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace RankingPokerHands
 {
@@ -9,15 +10,34 @@ namespace RankingPokerHands
         Tie 
     }
 
+    public enum HandRanking
+    {
+        RoyalStraightFlush = 10,
+        StraightFlush = 9
+    }
+
     public class PokerHand
     {
+        private readonly string[] _hand;
+        private readonly string[] _royalStraightMapper = {"T", "J", "Q", "K", "A"};
+        private readonly string[] _cardCompareMapper = { "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"};
+
         public PokerHand(string hand)
         {
+            _hand = hand.Split(" ").OrderBy(p => Array.IndexOf(_cardCompareMapper, p[0])).ToArray();
         }
 
         public Result CompareWith(PokerHand hand)
         {
-            throw new NotImplementedException();
+            return this.GetHandRanking() > hand.GetHandRanking() ? Result.Win : Result.Loss;
+        }
+
+        public HandRanking GetHandRanking()
+        {
+            return _hand.Select(p => p[1]).Distinct().Count() == 1 &&
+                   _hand.Select(p => p[0].ToString()).SequenceEqual<string>(_royalStraightMapper)
+                ? HandRanking.RoyalStraightFlush
+                : HandRanking.StraightFlush;
         }
     }
 }
