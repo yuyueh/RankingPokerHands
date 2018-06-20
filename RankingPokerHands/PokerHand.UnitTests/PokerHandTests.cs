081467652602506ef32a54b1ce7e399ea3e3abee
ed chang <ed.chang@xuenn.com>
2018-06-20 14:06:58 +0800

    Test_HighestStraightFlushWins

--- RankingPokerHands/PokerHand.UnitTests/PokerHandTests.cs
+++ RankingPokerHands/PokerHand.UnitTests/PokerHandTests.cs
@@ -6,8 +6,18 @@ namespace RankingPokerHands.UnitTests
     public class PokerHandTests
     {
         [Test]
-        public void FirstTest()
+        public void Test_HighestStraightFlushWins()
         {
+            // Arrange
+            var target = new PokerHand("2H 3H 4H 5H 6H");
+            var opponentHand = new PokerHand("KS AS TS QS JS");
+            var expected = Result.Loss;
+
+            // Act
+            var result = target.CompareWith(opponentHand);
+
+            // Assert
+            Assert.AreEqual(result, expected);
         }
     }
 }
