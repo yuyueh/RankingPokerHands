559a6d263dd7c4e8f2e8befce6c2addde9fe84e1
ed chang <ed.chang@xuenn.com>
2018-06-22 15:57:45 +0800

    Implement

--- RankingPokerHands/RankingPokerHands/PokerHand .cs
+++ RankingPokerHands/RankingPokerHands/PokerHand .cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace RankingPokerHands
 {
@@ -9,15 +10,38 @@ namespace RankingPokerHands
         Tie 
     }
 
+    public class Card
+    {
+        public char Name { get; set; }
+        public int Rank { get; set; }
+
+        public static Card Parse(string input)
+        {
+            return new Card
+            {
+                Name = input[0],
+                Rank = "23456789TJQK".IndexOf(input[0])
+            };
+        }
+    }
+
     public class PokerHand
     {
+        private List<Card> _hand;
+
         public PokerHand(string hand)
         {
+            _hand = hand.Split(' ').Select(Card.Parse).OrderByDescending(p => p.Rank).ToList();
         }
 
-        public Result CompareWith(PokerHand hand)
+        public List<Card> GetHand()
         {
-            throw new NotImplementedException();
+            return _hand;
+        }
+
+        public Result CompareWith(PokerHand opponentHand)
+        {
+            return _hand.First().Rank > opponentHand.GetHand().First().Rank ? Result.Win : Result.Loss;
         }
     }
 }
