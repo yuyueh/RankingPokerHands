a45381e4a7c050009fd02c1a39ab0c834111145c
ed chang <ed.chang@xuenn.com>
2018-06-26 10:40:53 +0800

    update

--- RankingPokerHands/RankingPokerHands/PokerHand .cs
+++ RankingPokerHands/RankingPokerHands/PokerHand .cs
@@ -30,59 +30,76 @@ namespace RankingPokerHands
 
     public class PokerHand
     {
-        private readonly int _rank;
-        private readonly int _baseScore;
+        private readonly List<Card> _hand;
+        private readonly List<Func<bool>> _compareList;
 
         public PokerHand(string handString)
         {
-            var hand = handString.Split(' ').Select(Card.Parse)
+            _hand = handString.Split(' ')
+                .Select(Card.Parse)
                 .OrderByDescending(p => p.Rank)
                 .ToList();
-            var comparesList = new List<Func<bool>>()
+
+            _compareList = new List<Func<bool>>()
             {
-                () => IsFlush(hand) &&
-                      IsStraight(hand) &&
-                      hand.Last().Name.Equals('A'),       // 皇家
-                () => IsFlush(hand) &&
-                      IsStraight(hand),                   // 同花順
-                () => IsSameValueWithTimes(hand, 4),      // 鐵支
-                () => IsSameValueWithTimes(hand, 3) &&
-                      IsSameValueWithTimes(hand, 2),      // Full House
-                () => IsFlush(hand),                      // Flush
-                () => IsStraight(hand),                   // 順
+                () => IsFlush() &&
+                      IsStraight() &&
+                      IsLastValueWith('A'), 
+                () => IsFlush() &&
+                      IsStraight(),
+                () => IsSameValueWithTimes(4),
+                () => IsSameValueWithTimes(3) &&
+                      IsSameValueWithTimes(2),
+                () => IsFlush(),
+                () => IsStraight(),
+                () => IsSameValueWithTimes(3),
+                () => IsTwoPair(),
+                () => IsSameValueWithTimes(2),
+                () => true
             };
-
-            _rank = (comparesList.Count - comparesList.FindIndex(f => f())) * (int)Math.Pow(10, 7);
-            _baseScore = hand.GroupBy(p => p.Rank)
-                .OrderByDescending(p => p.Count())
-                .ThenByDescending(p => p.Key)
-                .Select((p, i) => new {Value = p.Key, Index = i})
-                .Sum(p => p.Value * (int) Math.Pow(13, 5 - p.Index));
-        }
-
-        private bool IsSameValueWithTimes(List<Card> hand, int times)
-        {
-            return hand.GroupBy(p => p.Rank).Any(p => p.Count() == times);
-        }
-
-        private bool IsStraight(List<Card> hand)
-        {
-            return hand.Select((p, i) => p.Rank + i).Distinct().Count() == 1;
-        }
-
-        private bool IsFlush(List<Card> hand)
-        {
-            return hand.GroupBy(p => p.Suit).Count() == 1;
         }
 
         public int GetRank()
         {
-            return _rank + _baseScore;
+            var rank = (_compareList.Count - _compareList.FindIndex(f => f())) * (int) Math.Pow(10, 7);
+            var point = _hand.GroupBy(p => p.Rank)
+                .OrderByDescending(p => p.Count())
+                .ThenByDescending(p => p.Key)
+                .Select((p, i) => new {Value = p.Key, Index = i})
+                .Sum(p => p.Value * (int) Math.Pow(13, 5 - p.Index));
+            return rank + point;
         }
 
         public Result CompareWith(PokerHand opponentHand)
         {
-            return GetRank() > opponentHand.GetRank() ? Result.Win : Result.Loss;
+            return GetRank() > opponentHand.GetRank() ? 
+                Result.Win : GetRank() == opponentHand.GetRank() ? 
+                Result.Tie : Result.Loss;
+        }
+
+        private bool IsTwoPair()
+        {
+            return _hand.GroupBy(p => p.Rank).Count(p => p.Count() == 2) == 2;
+        }
+
+        private bool IsLastValueWith(char c)
+        {
+            return _hand.Last().Name.Equals(c);
+        }
+
+        private bool IsSameValueWithTimes(int times)
+        {
+            return _hand.GroupBy(p => p.Rank).Any(p => p.Count() == times);
+        }
+
+        private bool IsStraight()
+        {
+            return _hand.Select((p, i) => p.Rank + i).Distinct().Count() == 1;
+        }
+
+        private bool IsFlush()
+        {
+            return _hand.GroupBy(p => p.Suit).Count() == 1;
         }
     }
 }
